03b53874159e58f473e70b0bce5a9845264489b6
Dmitry Koroliov <d.koroliov@gmail.com>
2020-01-15 10:59:50 +0200

    changes in calc-angles
    accepts 100 percent
    completely new tests
    may not work with other modules in this commit

--- src/calc-angles-from-percents.js
+++ src/calc-angles-from-percents.js
@@ -3,25 +3,19 @@
 function calcAnglesFromPercents({percents, startAngle, counterClockwise}) {
   const onePercentAngle = Math.PI / 50;
   const angles = [startAngle];
-  let totalPercents = 0;
 
   if (counterClockwise) {
-    for (let i = 0; i < percents.length; i++) {
-      totalPercents += percents[i];
+    for (let i = 0; i < percents.length - 1; i++) {
       angles.push(angles[i] - onePercentAngle * percents[i]);
     }
   } else {
-    for (let i = 0; i < percents.length; i++) {
-      totalPercents += percents[i];
+    for (let i = 0; i < percents.length - 1; i++) {
       angles.push(angles[i] + onePercentAngle * percents[i]);
     }
   }
   angles.push(startAngle);
 
-  const retVal = Object.create(null);
-  retVal.error = totalPercents >= 100 ? 1 : 0;
-  retVal.angles = angles;
-  return retVal;
+  return angles;
 }
 
 module.exports = calcAnglesFromPercents;
--- test/calc-angles-from-percents.test.js
+++ test/calc-angles-from-percents.test.js
@@ -4,149 +4,173 @@ const tp = require('tape');
 const calcAnglesFromPercents = require('calc-angles-from-percents');
 const onePercentAngle = Math.PI / 50;
 
-testCounterClockwise();
-testClockwise();
-testErrors();
-
-tp('works if subsequent percent vals are smaller', t => {
+tp('clockwise, start 0', t => {
   t.plan(1);
-  const percents = [25, 3,];
-  const startAngle = 0.5;
-  t.deepEqual(
-    calcAnglesFromPercents({percents, startAngle, counterClockwise: false,}),
-    getExpectedRetValClockwise(percents, startAngle)
-  );
+  const percents = [10, 20, 70];
+  const startAngle = 0;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: false});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] + percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] + percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
 });
 
-tp('works with float percent vals', t => {
+tp('clockwise, start >0 <Pi, has vals >2Pi <start + 2Pi', t => {
   t.plan(1);
-  const percents = [25.0, 3.197,];
-  const startAngle = 0.5;
-  t.deepEqual(
-    calcAnglesFromPercents({percents, startAngle, counterClockwise: false,}),
-    getExpectedRetValClockwise(percents, startAngle)
-  );
+  const percents = [10, 20, 70];
+  const startAngle = Math.PI / 2;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: false});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] + percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] + percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
 });
 
-function testErrors() {
-  tp('returns an error, if >100 percent passed', t => {
-    t.plan(2);
-    checkErrors(t, [25, 100,], 1);
-  });
+tp('clockwise, start >0 <Pi, does not have vals >2Pi <start + 2Pi', t => {
+  t.plan(1);
+  const percents = [10, 20, 30];
+  const startAngle = Math.PI / 2;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: false});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] + percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] + percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
+});
 
-  tp('returns an error, if 100 percent passed', t => {
-    t.plan(2);
-    checkErrors(t, [25, 75,], 1);
-  });
-}
+tp('clockwise, start Pi', t => {
+  t.plan(1);
+  const percents = [10, 20, 70];
+  const startAngle = Math.Pi;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: false});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] + percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] + percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
+});
 
-function testClockwise() {
-  tp('works with a 0 start angle clockwise', t => {
-    t.plan(1);
-    const percents = [25, 39,];
-    const startAngle = 0;
-    t.deepEqual(
-      calcAnglesFromPercents({percents, startAngle, counterClockwise: false}),
-      getExpectedRetValClockwise(percents, startAngle)
-    );
-  });
+tp('clockwise, start >Pi <2Pi, has vals >3Pi <start + 2Pi', t => {
+  t.plan(1);
+  const percents = [10, 20, 70];
+  const startAngle = Math.PI + Math.PI / 2;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: false});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] + percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] + percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
+});
 
-  tp('works with a -0 start angle clockwise', t => {
-    t.plan(1);
-    const percents = [25, 39,];
-    const startAngle = -0;
-    t.deepEqual(
-      calcAnglesFromPercents({percents, startAngle, counterClockwise: false}),
-      getExpectedRetValClockwise(percents, startAngle)
-    );
-  });
+tp('clockwise, start >Pi <2Pi, does not have vals >3Pi <start + 2Pi', t => {
+  t.plan(1);
+  const percents = [10, 20, 30];
+  const startAngle = Math.PI + Math.PI / 2;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: false});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] + percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] + percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
+});
 
-  tp('works with a positive start angle clockwise', t => {
-    t.plan(1);
-    const percents = [25, 39,];
-    const startAngle = 0.5;
-    t.deepEqual(
-      calcAnglesFromPercents({percents, startAngle, counterClockwise: false}),
-      getExpectedRetValClockwise(percents, startAngle)
-    );
-  });
+tp('counter-clockwise, start 0', t => {
+  t.plan(1);
+  const percents = [10, 20, 70];
+  const startAngle = 0;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: true});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] - percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] - percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
+});
 
-  tp('works with a negative start angle clockwise', t => {
-    t.plan(1);
-    const percents = [25, 39,];
-    const startAngle = -0.5;
-    t.deepEqual(
-      calcAnglesFromPercents({percents, startAngle, counterClockwise: false}),
-      getExpectedRetValClockwise(percents, startAngle)
-    );
-  });
-}
+tp('counter-clockwise, start >0 <Pi, has vals <-Pi >start - 2Pi', t => {
+  t.plan(1);
+  const percents = [10, 20, 70];
+  const startAngle = Math.PI / 2;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: false});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] + percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] + percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
+});
 
-function testCounterClockwise() {
-  tp('works with a 0 start angle counter-clockwise', t => {
-    t.plan(1);
-    const percents = [25, 39,];
-    const startAngle = 0;
-    t.deepEqual(
-      calcAnglesFromPercents({percents, startAngle, counterClockwise: true,}),
-      getExpectedRetValCounterClockwise(percents, startAngle)
-    );
-  });
+tp('counter-clockwise, start >0 <Pi, does not have vals <-Pi >start - 2Pi',
+  t => {
+  t.plan(1);
+  const percents = [10, 20, 30];
+  const startAngle = Math.PI / 2;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: false});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] + percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] + percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
+});
 
-  tp('works with a -0 start angle counter-clockwise', t => {
-    t.plan(1);
-    const percents = [25, 39,];
-    const startAngle = -0;
-    t.deepEqual(
-      calcAnglesFromPercents({percents, startAngle, counterClockwise: true,}),
-      getExpectedRetValCounterClockwise(percents, startAngle)
-    );
-  });
+tp('counter-clockwise, start Pi', t => {
+  t.plan(1);
+  const percents = [10, 20, 70];
+  const startAngle = Math.PI;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: true});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] - percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] - percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
+});
 
-  tp('works with a positive start angle counter-clockwise', t => {
-    t.plan(1);
-    const percents = [25, 39,];
-    const startAngle = 0.5;
-    t.deepEqual(
-      calcAnglesFromPercents({percents, startAngle, counterClockwise: true,}),
-      getExpectedRetValCounterClockwise(percents, startAngle)
-    );
-  });
+tp('counter-clockwise, start >Pi <2Pi, has vals <0 >start - 2Pi', t => {
+  t.plan(1);
+  const percents = [10, 20, 70];
+  const startAngle = Math.PI + Math.PI / 2;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: false});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] + percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] + percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
+});
 
-  tp('works with a negative start angle counter-clockwise', t => {
-    t.plan(1);
-    const percents = [25, 39,];
-    const startAngle = -0.5;
-    t.deepEqual(
-      calcAnglesFromPercents({percents, startAngle, counterClockwise: true,}),
-      getExpectedRetValCounterClockwise(percents, startAngle)
-    );
-  });
-}
+tp('counter-clockwise, start >Pi <2Pi, does not have vals <0 >start - 2Pi',
+  t => {
+  t.plan(1);
+  const percents = [10, 20, 30];
+  const startAngle = Math.PI + Math.PI / 2;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: false});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] + percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] + percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
+});
 
-function checkErrors(t, percents, errorCode) {
-  const moduleRetVal =
-    calcAnglesFromPercents({percents, startAngle: 0, counterClockwise: true,});
-  t.equal(Object.getPrototypeOf(moduleRetVal), null);
-  t.equal(moduleRetVal.error, errorCode);
-}
-
-function getExpectedRetValClockwise(percents, startAngle) {
-  const expected = Object.create(null);
-  expected.error = 0;
-  expected.angles = [startAngle];
-  expected.angles[1] = onePercentAngle * percents[0] + startAngle;
-  expected.angles[2] = expected.angles[1] + onePercentAngle * percents[1];
-  expected.angles[3] = startAngle;
-  return expected;
-}
-
-function getExpectedRetValCounterClockwise(percents, startAngle) {
-  const expected = Object.create(null);
-  expected.error = 0;
-  expected.angles = [startAngle];
-  expected.angles[1] = startAngle - onePercentAngle * percents[0];
-  expected.angles[2] = expected.angles[1] - onePercentAngle * percents[1];
-  expected.angles[3] = startAngle;
-  return expected;
-}
+tp('has smaller values after bigger ones', t => {
+  t.plan(1);
+  const percents = [20, 10, 70];
+  const startAngle = 0;
+  const actualRetVal =
+    calcAnglesFromPercents({percents, startAngle, counterClockwise: false});
+  const expectedRetVal = [startAngle];
+  expectedRetVal[1] = expectedRetVal[0] + percents[0] * onePercentAngle;
+  expectedRetVal[2] = expectedRetVal[1] + percents[1] * onePercentAngle;
+  expectedRetVal[3] = startAngle;
+  t.deepEqual(actualRetVal, expectedRetVal);
+});
