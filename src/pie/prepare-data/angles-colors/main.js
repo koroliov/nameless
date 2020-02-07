f1e273f6fedcba40cfd999073a4fe9f1097dfe4e
Dmitry Koroliov <d.koroliov@gmail.com>
2020-02-07 16:04:05 +0200

    refactored prepare-data/angles-colors/main

--- src/pie/prepare-data/angles-colors/main.js
+++ src/pie/prepare-data/angles-colors/main.js
@@ -1,28 +1,15 @@
 'use strict';
 
 const filterZeroPercents = require('./filter-zero-percents');
-const calcAnglesFromPercents = require('./calc-angles-from-percents');
+const calculateAngles = require('./calculate-angles');
 const normalizeAngles = require('./normalize-angles');
 
-function main(args) {
-  const {percentsOriginal, colorsOriginal, startAngle, counterClockwise} = args;
-  const {colors, percents} =
-      filterZeroPercents(percentsOriginal, colorsOriginal);
-  if (colors.length === 1) {
-    return {
-      colors,
-      angles: [0],
-    };
+function main(data) {
+  filterZeroPercents(data);
+  if (data.get('colors').length !== 1) {
+    calculateAngles(data);
+    normalizeAngles(data);
   }
-  const angles = calcAnglesFromPercents({
-    percents,
-    startAngle,
-    counterClockwise,
-  });
-  return {
-    colors,
-    angles: normalizeAngles(angles),
-  };
 }
 
 module.exports = main;
--- test/unit/pie/prepare-data/angles-colors/main.test.js
+++ test/unit/pie/prepare-data/angles-colors/main.test.js
@@ -4,83 +4,77 @@ const tp = require('tape');
 const proxyquire = require('proxyquire').noCallThru();
 const sinon = require('sinon');
 
-tp('takes args, filters 0%, calculates angles, returns angles&percents', t => {
-  const colorsOriginal = Symbol('colors original');
-  const percentsOriginal = Symbol('percents original');
-  const startAngle = Symbol('start angle');
-  const counterClockwise = Symbol('counterClockwise flag');
+tp('takes data, filters 0%, calculates/normalizes angles, ' +
+  'does not change data by itself', t => {
+  const data = new Map([
+    ['colors', ['col 1', 'col 2']],
+  ]);
+  const spySet = sinon.spy(data, 'set');
+  const spyDelete = sinon.spy(data, 'delete');
+  const spyClear = sinon.spy(data, 'clear');
+  const mainDeps = {
+    './filter-zero-percents': sinon.fake(),
+    './calculate-angles': sinon.fake(),
+    './normalize-angles': sinon.fake(),
+  };
+  const main = proxyquire('pie/prepare-data/angles-colors/main', mainDeps);
+  main(data);
 
-  const filteredColors = Symbol('colors filtered');
-  const filteredPercents = Symbol('percents filtered');
-  const filterZeroPercentsStub = sinon.stub().returns({
-    colors: filteredColors,
-    percents: filteredPercents,
-  });
+  t.equal(mainDeps['./filter-zero-percents'].callCount, 1);
+  const filterZeroPercentsCall = mainDeps['./filter-zero-percents'].getCall(0);
+  t.equal(filterZeroPercentsCall.args.length, 1);
+  t.equal(filterZeroPercentsCall.args[0], data);
 
-  const calcAnglesFromPercentsRetVal = Symbol('angles calculated');
-  const calcAnglesFromPercentsStub =
-      sinon.stub().returns(calcAnglesFromPercentsRetVal);
+  t.equal(mainDeps['./calculate-angles'].callCount, 1);
+  const calculateAnglesCall = mainDeps['./calculate-angles'].getCall(0);
+  t.equal(calculateAnglesCall.args.length, 1);
+  t.equal(calculateAnglesCall.args[0], data);
+  t.equal(calculateAnglesCall.calledImmediatelyAfter(filterZeroPercentsCall),
+      true);
 
-  const normalizeAnglesRetVal = Symbol('angles normalized');
-  const normalizeAnglesStub = sinon.stub().returns(normalizeAnglesRetVal);
+  t.equal(mainDeps['./normalize-angles'].callCount, 1);
+  const normalizeAnglesCall = mainDeps['./normalize-angles'].getCall(0);
+  t.equal(normalizeAnglesCall.args.length, 1);
+  t.equal(normalizeAnglesCall.args[0], data);
+  t.equal(normalizeAnglesCall.calledImmediatelyAfter(calculateAnglesCall),
+      true);
 
-  const main = proxyquire('pie/prepare-angles-n-colors/main', {
-    './filter-zero-percents': filterZeroPercentsStub,
-    './calc-angles-from-percents': calcAnglesFromPercentsStub,
-    './normalize-angles': normalizeAnglesStub,
-  });
-
-  const {colors, angles} =
-      main({percentsOriginal, colorsOriginal, startAngle, counterClockwise});
-  t.equal(colors, filteredColors);
-  t.equal(angles, normalizeAnglesRetVal);
-
-  t.equal(filterZeroPercentsStub.callCount, 1);
-  t.deepEqual(filterZeroPercentsStub.getCall(0).args,
-      [percentsOriginal, colorsOriginal]);
-
-  t.equal(calcAnglesFromPercentsStub.callCount, 1);
-  t.deepEqual(calcAnglesFromPercentsStub.getCall(0).args,
-    [{
-      percents: filteredPercents,
-      startAngle,
-      counterClockwise,
-    }]
-  );
-
-  t.equal(normalizeAnglesStub.callCount, 1);
-  t.deepEqual(normalizeAnglesStub.getCall(0).args,
-    [calcAnglesFromPercentsRetVal]
-  );
+  t.equal(spySet.callCount, 0);
+  t.equal(spyDelete.callCount, 0);
+  t.equal(spyClear.callCount, 0);
 
   t.end();
 });
 
-tp('in case of 100% present it returns single color and angle [0]', t => {
-  const colorsOriginal = Symbol('colors original');
-  const percentsOriginal = Symbol('percents original');
-  const startAngle = Symbol('start angle');
-  const counterClockwise = Symbol('counterClockwise flag');
-
+tp('in case of 100% present it does not calculate angles, ' +
+  'does not change data by itself', t => {
+  const data = new Map([
+    ['colors', ['col 1', 'col 2']],
+  ]);
+  const spySet = sinon.spy(data, 'set');
+  const spyDelete = sinon.spy(data, 'delete');
+  const spyClear = sinon.spy(data, 'clear');
   const mainDeps = {
-    './filter-zero-percents': sinon.stub().returns({
-      colors: ['single col'],
-      percents: [100],
+    './filter-zero-percents': sinon.spy((data) => {
+      data.set('colors', ['col 1']);
     }),
-    './calc-angles-from-percents': sinon.fake(),
+    './calculate-angles': sinon.fake(),
     './normalize-angles': sinon.fake(),
   };
-  const main = proxyquire('pie/prepare-angles-n-colors/main', mainDeps);
-
-  const actual =
-      main({percentsOriginal, colorsOriginal, startAngle, counterClockwise});
-  t.deepEqual(actual, {angles: [0], colors: ['single col']});
+  const main = proxyquire('pie/prepare-data/angles-colors/main', mainDeps);
+  main(data);
 
   t.equal(mainDeps['./filter-zero-percents'].callCount, 1);
-  t.deepEqual(mainDeps['./filter-zero-percents'].getCall(0).args,
-      [percentsOriginal, colorsOriginal]);
-  t.equal(mainDeps['./calc-angles-from-percents'].callCount, 0);
+  const filterZeroPercentsCall = mainDeps['./filter-zero-percents'].getCall(0);
+  t.equal(filterZeroPercentsCall.args.length, 1);
+  t.equal(filterZeroPercentsCall.args[0], data);
+
+  t.equal(mainDeps['./calculate-angles'].callCount, 0);
   t.equal(mainDeps['./normalize-angles'].callCount, 0);
+
+  t.equal(spySet.callCount, 1, 'the one from the filterZeroPercents spy');
+  t.equal(spyDelete.callCount, 0);
+  t.equal(spyClear.callCount, 0);
 
   t.end();
 });
