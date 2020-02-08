c864cbcaaf4660d7abda259a3f2ff6875a6d2d3e
Dmitry Koroliov <d.koroliov@gmail.com>
2020-02-08 03:13:25 +0200

    refactored apply-context-state-changes

--- src/pie/apply-context-state-changes.js
+++ src/pie/apply-context-state-changes.js
@@ -1,16 +1,19 @@
 'use strict';
 
-function applyContextStateChanges(args) {
-  const {cntx, ox, oy, strokeColor, strokeWidth, rotationAngle, scaleY} = args;
+function applyContextStateChanges(data) {
+  const cntx = data.get('cntx');
+  const centerX = data.get('centerX');
+  const centerY = data.get('centerY');
+  const strokeWidth = data.get('strokeWidth');
+
   if (strokeWidth !== 0) {
-    cntx.strokeStyle = strokeColor;
+    cntx.strokeStyle = data.get('strokeColor');
     cntx.lineWidth = strokeWidth;
   }
-
-  cntx.translate(ox, oy);
-  cntx.rotate(rotationAngle);
-  cntx.scale(1, scaleY);
-  cntx.translate(-ox, -oy);
+  cntx.translate(centerX, centerY);
+  cntx.rotate(data.get('rotationAngle'));
+  cntx.scale(1, data.get('scaleY'));
+  cntx.translate(-centerX, -centerY);
 }
 
 module.exports = applyContextStateChanges;
--- test/unit/pie/apply-context-state-changes.test.js
+++ test/unit/pie/apply-context-state-changes.test.js
@@ -4,8 +4,8 @@ const tp = require('tape');
 const sinon = require('sinon');
 const applyContextStateChanges = require('pie/apply-context-state-changes');
 
-tp('applies styles, translation, rotation, scaling in the right order', t => {
-  t.plan(13);
+tp(`applies styles, translation, rotation, scaling in the right order,
+it does not change data in any way`, t => {
   const mockContext2d = {
     translate: sinon.fake(),
     rotate: sinon.fake(),
@@ -14,43 +14,53 @@ tp('applies styles, translation, rotation, scaling in the right order', t => {
     lineWidth: undefined,
   };
   const originalKeysLength = Object.keys(mockContext2d).length;
-  const argument = {
-    cntx: mockContext2d,
-    ox: 1,
-    oy: 2,
-    strokeColor: Symbol('strokeColor'),
-    strokeWidth: 1,
-    rotationAngle: Symbol('rotationAngle'),
-    scaleY: Symbol('scaleY'),
-  };
-  applyContextStateChanges(argument);
+  const data = new Map([
+    ['cntx', mockContext2d],
+    ['centerX', 1],
+    ['centerY', 2],
+    ['strokeColor', Symbol('strokeColor')],
+    ['strokeWidth', 1],
+    ['rotationAngle', Symbol('rotationAngle')],
+    ['scaleY', Symbol('scaleY')],
+  ]);
+  const spySet = sinon.spy(data, 'set');
+  const spyDelete = sinon.spy(data, 'delete');
+  const spyClear = sinon.spy(data, 'clear');
+  applyContextStateChanges(data);
 
   const actualKeysLength = Object.keys(mockContext2d).length;
   t.equal(actualKeysLength, originalKeysLength, 'no new props were added');
-  t.equal(mockContext2d.strokeStyle, argument.strokeColor);
-  t.equal(mockContext2d.lineWidth, argument.strokeWidth);
+  t.equal(mockContext2d.strokeStyle, data.get('strokeColor'));
+  t.equal(mockContext2d.lineWidth, data.get('strokeWidth'));
 
   t.equal(mockContext2d.translate.callCount, 2);
   const firstTranslateCall = mockContext2d.translate.getCall(0);
-  t.deepEqual(firstTranslateCall.args, [argument.ox, argument.oy]);
+  t.deepEqual(firstTranslateCall.args,
+      [data.get('centerX'), data.get('centerY')]);
 
   t.equal(mockContext2d.rotate.callCount, 1);
   const rotateCall = mockContext2d.rotate.getCall(0);
   t.equal(rotateCall.calledImmediatelyAfter(firstTranslateCall), true);
-  t.deepEqual(rotateCall.args, [argument.rotationAngle]);
+  t.deepEqual(rotateCall.args, [data.get('rotationAngle')]);
 
   t.equal(mockContext2d.scale.callCount, 1);
   const scaleCall = mockContext2d.scale.getCall(0);
   t.equal(scaleCall.calledImmediatelyAfter(rotateCall), true);
-  t.deepEqual(scaleCall.args, [1, argument.scaleY]);
+  t.deepEqual(scaleCall.args, [1, data.get('scaleY')]);
 
   const secondTranslateCall = mockContext2d.translate.getCall(1);
   t.equal(secondTranslateCall.calledImmediatelyAfter(scaleCall), true);
-  t.deepEqual(secondTranslateCall.args, [-argument.ox, -argument.oy]);
+  t.deepEqual(secondTranslateCall.args,
+      [-data.get('centerX'), -data.get('centerY')]);
+
+  t.equal(spySet.callCount, 0);
+  t.equal(spyDelete.callCount, 0);
+  t.equal(spyClear.callCount, 0);
+
+  t.end();
 });
 
 tp('if strokeWidth is 0, strokeColor, strokeWidth are not applied', t => {
-  t.plan(13);
   const originalStrokeStyle = Symbol('original strokeStyle');
   const originalLineWidth = Symbol('original lineWidth');
   const mockContext2d = {
@@ -61,16 +71,16 @@ tp('if strokeWidth is 0, strokeColor, strokeWidth are not applied', t => {
     lineWidth: originalLineWidth,
   };
   const originalKeysLength = Object.keys(mockContext2d).length;
-  const argument = {
-    cntx: mockContext2d,
-    ox: 1,
-    oy: 2,
-    strokeColor: Symbol('strokeColor'),
-    strokeWidth: 0,
-    rotationAngle: Symbol('rotationAngle'),
-    scaleY: Symbol('scaleY'),
-  };
-  applyContextStateChanges(argument);
+  const data = new Map([
+    ['cntx', mockContext2d],
+    ['centerX', 1],
+    ['centerY', 2],
+    ['strokeColor', Symbol('strokeColor')],
+    ['strokeWidth', 0],
+    ['rotationAngle', Symbol('rotationAngle')],
+    ['scaleY', Symbol('scaleY')],
+  ]);
+  applyContextStateChanges(data);
 
   const actualKeysLength = Object.keys(mockContext2d).length;
   t.equal(actualKeysLength, originalKeysLength, 'no new props were added');
@@ -79,19 +89,23 @@ tp('if strokeWidth is 0, strokeColor, strokeWidth are not applied', t => {
 
   t.equal(mockContext2d.translate.callCount, 2);
   const firstTranslateCall = mockContext2d.translate.getCall(0);
-  t.deepEqual(firstTranslateCall.args, [argument.ox, argument.oy]);
+  t.deepEqual(firstTranslateCall.args,
+      [data.get('centerX'), data.get('centerY')]);
 
   t.equal(mockContext2d.rotate.callCount, 1);
   const rotateCall = mockContext2d.rotate.getCall(0);
   t.equal(rotateCall.calledImmediatelyAfter(firstTranslateCall), true);
-  t.deepEqual(rotateCall.args, [argument.rotationAngle]);
+  t.deepEqual(rotateCall.args, [data.get('rotationAngle')]);
 
   t.equal(mockContext2d.scale.callCount, 1);
   const scaleCall = mockContext2d.scale.getCall(0);
   t.equal(scaleCall.calledImmediatelyAfter(rotateCall), true);
-  t.deepEqual(scaleCall.args, [1, argument.scaleY]);
+  t.deepEqual(scaleCall.args, [1, data.get('scaleY')]);
 
   const secondTranslateCall = mockContext2d.translate.getCall(1);
   t.equal(secondTranslateCall.calledImmediatelyAfter(scaleCall), true);
-  t.deepEqual(secondTranslateCall.args, [-argument.ox, -argument.oy]);
+  t.deepEqual(secondTranslateCall.args,
+      [-data.get('centerX'), -data.get('centerY')]);
+
+  t.end();
 });
